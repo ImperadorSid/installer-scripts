3c33002386f1348e6a904a0bead476d075c230d2
Moisés Carvalho <moisescarv27@hotmail.com>
2020-05-29 14:04:27 -0300

    Remove Spotify

--- Apps/Spotify/spotify.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-curl -L https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt update
-sudo apt install -y spotify-client
