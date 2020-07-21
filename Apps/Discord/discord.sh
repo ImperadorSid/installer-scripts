2f98e63faef5d32fdd16afa8bb19f7cb7f5f980b
Moisés Carvalho <moisescarv27@hotmail.com>
2020-07-21 14:56:38 -0300

    Add Discord script

---
+++ Apps/Discord/discord.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+DISCORD_PACKAGE=discord*deb
+
+sudo apt install -y ./$DISCORD_PACKAGE
--- Apps/Folding@home/fah.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-find -name '*deb' -exec sudo apt install -y {} \;
