50fdaa5a8115c80db2c34e878958f95143c8a8bb
Moisés Carvalho <moisescarv27@hotmail.com>
2020-07-24 01:25:54 -0300

    Remove Dart, Flutter, Python and C++ extensions

--- Development/Code/vscode.sh
+++ Development/Code/vscode.sh
@@ -5,10 +5,7 @@ aria2c "https://update.code.visualstudio.com/latest/linux-deb-x64/stable"
 VSCODE_PACKAGE=code*
 
 sudo apt install -y ./$VSCODE_PACKAGE
-code --install-extension Dart-Code.flutter
 code --install-extension dracula-theme.theme-dracula
-code --install-extension ms-python.python
-code --install-extension ms-vscode.cpptools
 code --install-extension pkief.material-icon-theme
 code --install-extension ritwickdey.liveserver
 code --install-extension skyapps.fish-vscode
