068f503aa114bfa19c09a5a15c1dadc09ea32d81
Moisés Carvalho <moisescarv27@hotmail.com>
2020-07-26 15:06:18 -0300

    Update NodeJS script to use Yarn as package manager

--- Development/NodeJS/nodejs.sh
+++ Development/NodeJS/nodejs.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
-rm -f node*z
-LATEST_VERSION=$(curl -s https://nodejs.org/dist/index.json | jq -r '.[0].version')
+NODE_ROOT=~/.local/node
+NODE_BIN=$NODE_ROOT/bin
+YARN_BIN=~/.yarn/bin
 
+rm -rf node*z $NODE_ROOT ~/.npm ~/.{,{cache,config}/}yarn*
+
+LATEST_VERSION=$(curl -s https://nodejs.org/dist/index.json | jq -r '.[0].version')
 aria2c "https://nodejs.org/dist/latest/node-$LATEST_VERSION-linux-x64.tar.xz"
 
 tar xvf node-$LATEST_VERSION-linux-x64.tar.xz -C ~/.local --one-top-level=node --strip-components=1
 
 CUR_PATH=$(fish -c 'echo $fish_user_paths')
-NODE_BIN=/home/impsid/.local/node/bin
 [[ "$CUR_PATH" != *"$NODE_BIN"* ]] && fish -c "set -p fish_user_paths $NODE_BIN"
+[[ "$CUR_PATH" != *"$YARN_BIN"* ]] && fish -c "set -p fish_user_paths $YARN_BIN"
 
 PATH=$NODE_BIN:$PATH
-$NODE_BIN/npm i -g $(cat packages.txt)
+$NODE_BIN/npm install --global yarn
+$NODE_BIN/yarn global add $(cat packages.txt)
 
