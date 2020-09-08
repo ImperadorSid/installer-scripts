878668809eb030304448557bc158f6881df4a740
Moisés Carvalho <moisescarv27@hotmail.com>
2020-09-08 13:15:08 -0300

    Reorder extensiosn

--- Development/Code/vscode.sh
+++ Development/Code/vscode.sh
@@ -5,11 +5,11 @@ aria2c "https://update.code.visualstudio.com/latest/linux-deb-x64/stable"
 VSCODE_PACKAGE=code*
 
 sudo apt install -y ./$VSCODE_PACKAGE
+code --install-extension Angular.ng-template
+code --install-extension dbaeumer.vscode-eslint 
 code --install-extension dracula-theme.theme-dracula
+code --install-extension ms-vsliveshare.vsliveshare
 code --install-extension pkief.material-icon-theme
 code --install-extension ritwickdey.liveserver
 code --install-extension skyapps.fish-vscode
-code --install-extension Angular.ng-template
-code --instal-extension ms-vsliveshare.vsliveshare
-code --install-extension dbaeumer.vscode-eslint 
 
