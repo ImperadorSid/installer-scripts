c0266f46858a4109450e71db538ad911455cbd71
Moisés Carvalho <moisescarv27@hotmail.com>
2020-09-24 16:17:52 -0300

    Add install script for MongoDB

---
+++ Development/MongoDB/mongodb.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+
+apt update
+apt install -y mongodb-org
+
+sudo systemctl start mongod
+sudo systemctl status mongod
+
+
