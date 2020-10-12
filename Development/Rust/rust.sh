e99169a3d047a853ecfd1588f45cf5bfd6b7353d
Moisés Carvalho <moisescarv27@hotmail.com>
2020-10-12 15:17:42 -0300

    Remove alacritty from package list

--- Development/Rust/rust.sh
+++ Development/Rust/rust.sh
@@ -6,8 +6,5 @@ CUR_PATH=$(fish -c 'echo $fish_user_paths')
 CARGO_BIN=~/.cargo/bin
 [[ "$CUR_PATH" != *"$CARGO_BIN"* ]] && fish -c "set -p fish_user_paths $CARGO_BIN"
 
-# Prerequisites for "alacritty"
-sudo apt install cmake libfontconfig-dev libxcb-{render,shape,xfixes}0-dev
-
 $CARGO_BIN/cargo install $(cat packages.txt)
 
