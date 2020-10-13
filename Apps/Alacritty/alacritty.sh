58853836563a63e2b9b9a8e25f02905de2696f1a
Moisés Carvalho <moisescarv27@hotmail.com>
2020-10-13 16:47:19 -0300

    Add VS Code extension for path completion

--- Apps/Alacritty/alacritty.sh
+++ Apps/Alacritty/alacritty.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-sudo apt install cmake libfontconfig-dev libxcb-{render,shape,xfixes}0-dev
+sudo apt install -y cmake libfontconfig-dev libxcb-{render,shape,xfixes}0-dev
 
 cargo install alacritty
 
-sudo apt remove {gnome,mate}-terminal
+sudo apt purge -y {gnome,mate}-terminal
 sudo ln -sf ~/.local/scripts/alacritty-wrapper.fish /usr/bin/gnome-terminal
 sudo ln -sf ~/.local/scripts/alacritty-wrapper.fish /usr/bin/mate-terminal
 
