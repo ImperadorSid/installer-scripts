2647a347b690287efdb8e0b7654075f91c7bcc6d
Moisés Carvalho <moisescarv27@hotmail.com>
2020-10-15 16:50:01 -0300

    Update Android and Flutter installation paths

--- Development/Android/android.sh
+++ Development/Android/android.sh
@@ -8,9 +8,10 @@ mkdir -p $INSTALL_PATH
 
 unzip $CMDLINETOOLS_ARCHIVE -d $INSTALL_PATH
 
-ln -s $INSTALL_PATH/tools/bin/sdkmanager ~/.local/bin
-ln -s $INSTALL_PATH/tools/bin/avdmanager ~/.local/bin
-ln -s $SDK_HOME/emulator/emulator ~/.local/bin
+ln -sf $INSTALL_PATH/tools/bin/sdkmanager ~/.local/bin
+ln -sf $INSTALL_PATH/tools/bin/avdmanager ~/.local/bin
+ln -sf $SDK_HOME/emulator/emulator ~/.local/bin
 
-$INSTALL_PATH/tools/bin/sdkmanager "emulator" "system-images;android-30;google_apis;x86"
+$INSTALL_PATH/tools/bin/sdkmanager "emulator" "system-images;android-30;google_apis;x86" "platform-tools"
+mkdir $SDK_HOME/platforms
 
--- Development/Flutter/flutter.sh
+++ Development/Flutter/flutter.sh
@@ -1,19 +1,13 @@
 #!/usr/bin/env bash
 
 INSTALL_DIR=~/.local
-BASIC_TOOLS_PATH=$INSTALL_DIR/android-sdk/cmdline-tools
-CMDLINETOOLS_ARCHIVE=command*
 FLUTTER_ARCHIVE=flutter_linux*
 USER_BIN_DIR=~/.local/bin
 
-mkdir -p $BASIC_TOOLS_PATH
 
 tar xvf $FLUTTER_ARCHIVE -C $INSTALL_DIR
-unzip $CMDLINETOOLS_ARCHIVE -d $BASIC_TOOLS_PATH
-mv $BASIC_TOOLS_PATH/tools $BASIC_TOOLS_PATH/latest
 
-ln -s ~/.local/android-sdk/platform-tools/adb $USER_BIN_DIR
-ln -s ~/.local/android-sdk/cmdline-tools/latest/bin/sdkmanager $USER_BIN_DIR
 ln -s ~/.local/flutter/bin/flutter $USER_BIN_DIR
 
-$BASIC_TOOLS_PATH/latest/bin/sdkmanager "build-tools;30.0.2" "emulator" "platform-tools" "platforms;android-30" "system-images;android-30;google_apis;x86"
+$BASIC_TOOLS_PATH/latest/bin/sdkmanager "build-tools;30.0.2" "platforms;android-30"
+
