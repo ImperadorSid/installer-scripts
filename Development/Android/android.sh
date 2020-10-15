e947bc1e8f81417fa5566a7502fa8d43c824846c
Moisés Carvalho <moisescarv27@hotmail.com>
2020-10-15 16:53:42 -0300

    Add link to ADB

--- Development/Android/android.sh
+++ Development/Android/android.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
 SDK_HOME=~/.local/android-sdk
-INSTALL_PATH=$SDK_HOME/cmdline-tools
+CLI_PATH=$SDK_HOME/cmdline-tools
 CMDLINETOOLS_ARCHIVE=command*
 
-mkdir -p $INSTALL_PATH
+mkdir -p $CLI_PATH
 
-unzip $CMDLINETOOLS_ARCHIVE -d $INSTALL_PATH
+unzip $CMDLINETOOLS_ARCHIVE -d $CLI_PATH
 
-ln -sf $INSTALL_PATH/tools/bin/sdkmanager ~/.local/bin
-ln -sf $INSTALL_PATH/tools/bin/avdmanager ~/.local/bin
+ln -sf $CLI_PATH/tools/bin/sdkmanager ~/.local/bin
+ln -sf $CLI_PATH/tools/bin/avdmanager ~/.local/bin
 ln -sf $SDK_HOME/emulator/emulator ~/.local/bin
+ln -sf $SDK_HOME/platform-tools/adb ~/.local/bin
 
-$INSTALL_PATH/tools/bin/sdkmanager "emulator" "system-images;android-30;google_apis;x86" "platform-tools"
-mkdir $SDK_HOME/platforms
+$CLI_PATH/tools/bin/sdkmanager "emulator" "system-images;android-30;google_apis;x86" "platform-tools"
+mkdir -p $SDK_HOME/platforms
 
