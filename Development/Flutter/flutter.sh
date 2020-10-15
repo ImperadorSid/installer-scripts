1042991cb43544af0cf7132ce8e2a92671b413ef
Moisés Carvalho <moisescarv27@hotmail.com>
2020-10-15 16:14:32 -0300

    Update SDK Manager packages

--- Development/Flutter/flutter.sh
+++ Development/Flutter/flutter.sh
@@ -16,4 +16,4 @@ ln -s ~/.local/android-sdk/platform-tools/adb $USER_BIN_DIR
 ln -s ~/.local/android-sdk/cmdline-tools/latest/bin/sdkmanager $USER_BIN_DIR
 ln -s ~/.local/flutter/bin/flutter $USER_BIN_DIR
 
-$BASIC_TOOLS_PATH/latest/bin/sdkmanager "build-tools;28.0.3" "emulator" "platform-tools" "platforms;android-28" "system-images;android-29;google_apis;x86"
+$BASIC_TOOLS_PATH/latest/bin/sdkmanager "build-tools;30.0.2" "emulator" "platform-tools" "platforms;android-30" "system-images;android-30;google_apis;x86"
