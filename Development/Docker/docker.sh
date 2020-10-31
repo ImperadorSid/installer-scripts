e0f3edaba2697bf9729c5d05bca4560ab5b83f61
Moisés Carvalho <moisescarv27@hotmail.com>
2020-10-31 11:42:15 -0300

    Add Docker install script

---
+++ Development/Docker/docker.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+
